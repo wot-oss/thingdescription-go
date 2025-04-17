a3b71a00c8cfadf9eb767e23033aa732d1141f27
Jozef Kralik <jozef.kralik@plgd.dev>
2025-04-17 14:46:41 +0200

    Add AdditionalFields to link

--- thingDescription/IconLinkElement.go
+++ thingDescription/IconLinkElement.go
@@ -1,10 +1,11 @@
 package thingDescription
 
 type IconLinkElement struct {
-	Anchor   *string   `json:"anchor,omitempty"`
-	Href     string    `json:"href"`
-	Hreflang *Hreflang `json:"hreflang,omitempty"`
-	Rel      *string   `json:"rel,omitempty"`
-	Type     *string   `json:"type,omitempty"`
-	Sizes    *string   `json:"sizes,omitempty"`
+	Anchor           *string                `json:"anchor,omitempty"`
+	Href             string                 `json:"href"`
+	Hreflang         *Hreflang              `json:"hreflang,omitempty"`
+	Rel              *string                `json:"rel,omitempty"`
+	Type             *string                `json:"type,omitempty"`
+	Sizes            *string                `json:"sizes,omitempty"`
+	AdditionalFields map[string]interface{} `json:",unknown"`
 }
